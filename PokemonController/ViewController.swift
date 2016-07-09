c0b8f4fd097c525ed88e75a22294b1e42b4f01b1
Johnson Kaho Poon <kahopoon@gmail.com>
2016-07-09 13:46:02 +0800

    game controller open last known location when initial open, try to make moving record consistent

--- PokemonController/ViewController.swift
+++ PokemonController/ViewController.swift
@@ -19,11 +19,15 @@ class ViewController: UIViewController, MKMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        getSavedLocation() ? showMapOnLocation() : ()
+        
         startWebServer()
     }
 
     func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
         currentLocation = mapView.centerCoordinate
+        saveLocation()
     }
 
     func changeCurrentLocation(direction:String) {
@@ -33,11 +37,33 @@ class ViewController: UIViewController, MKMapViewDelegate {
         direction == "up" ? currentLocation = CLLocationCoordinate2D(latitude: currentLocation.latitude + moveInterval, longitude: currentLocation.longitude) : ()
         direction == "down" ? currentLocation = CLLocationCoordinate2D(latitude: currentLocation.latitude - moveInterval, longitude: currentLocation.longitude) : ()
         
+        saveLocation()
+        showMapOnLocation()
+    }
+    
+    func showMapOnLocation() {
         mapView.setCamera(MKMapCamera(lookingAtCenterCoordinate: currentLocation, fromEyeCoordinate: currentLocation, eyeAltitude: 500.0), animated: false)
+    }
+    
+    func saveLocation() {
+        NSUserDefaults.standardUserDefaults().setObject(getCurrentLocationDict(), forKey: "savedLocation")
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    func getSavedLocation() -> Bool {
+        guard let savedLocation = NSUserDefaults.standardUserDefaults().objectForKey("savedLocation") else {
+            return false
+        }
+        return putCurrentLocationFromDict(savedLocation as! [String : String])
     }
     
     func getCurrentLocationDict() -> [String:String] {
         return ["lat":"\(currentLocation.latitude)", "lng":"\(currentLocation.longitude)"]
+    }
+    
+    func putCurrentLocationFromDict(dict: [String:String]) -> Bool {
+        currentLocation = CLLocationCoordinate2D(latitude: Double(dict["lat"]!)!, longitude: Double(dict["lng"]!)!)
+        return true
     }
     
     @IBAction func moveUp(sender: AnyObject) {
