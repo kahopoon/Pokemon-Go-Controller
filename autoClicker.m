dba971d5298a97a40c2e048e6504c48bf35b7045
taromati <2logta@gmail.com>
2016-07-12 22:08:49 +0900

    Change CGPostMouseEvent to CGEventCreateMouseEvent

--- autoClicker.m
+++ autoClicker.m
@@ -1,20 +1,28 @@
 #import <Foundation/Foundation.h>
 #import <ApplicationServices/ApplicationServices.h>
 
+void postMouseEvent(CGMouseButton button, CGEventType type, const CGPoint point) {
+    CGEventRef theEvent = CGEventCreateMouseEvent(NULL, type, point, button);
+    CGEventSetType(theEvent, type);
+    CGEventPost(kCGHIDEventTap, theEvent);
+    CFRelease(theEvent);
+}
+
 int main(int argc, char *argv[]) {
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
-
-    int x = [args integerForKey:@"x"];
-    int y = [args integerForKey:@"y"];
-
-    CGPoint pt;
-    pt.x = x;
-    pt.y = y;
-
-    CGPostMouseEvent( pt, 1, 1, 1 );
-    CGPostMouseEvent( pt, 1, 1, 0 );
-
-    [pool release];
+    @autoreleasepool {
+        NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
+        
+        int x = [args integerForKey:@"x"];
+        int y = [args integerForKey:@"y"];
+        
+        CGPoint pt;
+        pt.x = x;
+        pt.y = y;
+        
+        postMouseEvent(kCGMouseButtonLeft, kCGEventLeftMouseDown, CGPointMake(x, y));
+        usleep(100 * 1000);
+        postMouseEvent(kCGMouseButtonLeft, kCGEventLeftMouseUp, CGPointMake(x, y));
+    }
+    
     return 0;
 }
